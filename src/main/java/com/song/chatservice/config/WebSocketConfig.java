3c233445d726943c8b86b7744af5b847a3bc3d23
Song-Hyunsung <song.hyunsung96@gmail.com>
2020-06-26 13:01:22 -0400

    add CORS to allow all

--- src/main/java/com/song/chatservice/config/WebSocketConfig.java
+++ src/main/java/com/song/chatservice/config/WebSocketConfig.java
@@ -2,6 +2,7 @@ package com.song.chatservice.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
@@ -17,7 +18,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     // Websocket is communication protocol, STOMP provides simple messaging protocol
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry){
-        registry.addEndpoint("/ws").withSockJS();
+        registry.addEndpoint("/ws").setAllowedOrigins("*").withSockJS();
     }
 
     // This method configures message broker that will be used to route messages from one to another
